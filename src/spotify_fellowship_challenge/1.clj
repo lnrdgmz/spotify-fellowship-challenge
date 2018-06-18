5699e39af64423a897f9fc48261b1fe8c979b5bb
Leo Gomez <leogomez@gmail.com>
2018-06-18 15:39:09 -0400

    Add comments to part 1

--- src/spotify_fellowship_challenge/1.clj
+++ src/spotify_fellowship_challenge/1.clj
@@ -1,8 +1,13 @@
 (ns spotify-fellowship-challenge.1
   (:require [clojure.string :as str]))
 
-(defn make-comparator [ord]
+;; We can use the built-in `sort` function with a custom comparator. Our
+;; `make-compatator` function will return a function that will compare two
+;; characters based on their position in the string `ord`.
+
+(defn- make-comparator
   "Returns a comparator function based on the specified ordering `ord`."
+  [ord]
   (fn [a b]
     (compare (str/index-of ord a)
              (str/index-of ord b))))
@@ -10,3 +15,14 @@
 (defn sort-by-string [str ord]
   (let [comparator (make-comparator ord)]
     (str/join (sort comparator str))))
+
+
+;; This approach is slightly inefficient, with a time complexity of
+;; O(n^2*log(n)). The `sort` function is O(nlog(n)), but the comparator function
+;; is linear, because it uses `index-of` to compare individual characters.
+;;
+;; The current code works acceptably for the given examples, but if we needed
+;; improved performance with regard to time, we can make a constant-time
+;; comparator. By iterating through `ord` once, we can build up a hash table
+;; with char/index pairs. Our comparator could then compare characters in
+;; constant time, making `sort-by-string` have a time complexity of O(nlog(n)).
